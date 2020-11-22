6772a02d24305b49d11c03cfe4be19e29793c37c
Namelessafk <66872553+Namelessafk@users.noreply.github.com>
2020-11-22 14:05:47 -0200

    Reconfiguração do caminho de rotas e complementação da atividade
    Parte não finalizada da ultima aula em questão de aproveitamento do caminho de rotas pelo erro do id e pela falta de 1 pagina.

--- upa/forms.py
+++ upa/forms.py
@@ -4,4 +4,4 @@ from .models import Upa
 class UpaForm(forms.ModelForm):
     class Meta:
         model = Upa
-        fields = ['cep','endereço','nome','telefone','especificacoes','descricao']
+        fields = ['cep','nome','endereco','telefone','especificacoes','descricao']
--- upa/views.py
+++ upa/views.py
@@ -6,14 +6,14 @@ def list_upa(request):
     upas = Upa.objects.all()
     return render(request, 'upa.html', {'upas': upas})
 
-    def create_upa(request):
-        form = UpaForm(request.POST or None)
+def create_upa(request):
+    form = UpaForm(request.POST or None)
 
-        if form.is_valid():
-            form.save()
-            return redirect('list_upa')
+    if form.is_valid():
+        form.save()
+        return redirect('list_upa')
 
-        return render(request, 'upa-form.html', {'form': form})
+    return render(request, 'upa-form.html', {'form': form})
 
 def update_upa(request,cep):
     upa = Upa.objects.get(cep=cep)
